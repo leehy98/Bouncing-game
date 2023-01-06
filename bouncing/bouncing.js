c28d629b68a9baa2aec539c1d4ea41557ed66aa0
leehy98 <117696170+leehy98@users.noreply.github.com>
2023-01-06 18:12:16 +0900

    Add files via upload

--- bouncing/bouncing.js
+++ bouncing/bouncing.js
@@ -1,3 +1,4 @@
+//정의된 변수값
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x= canvas.width/2;
@@ -7,8 +8,33 @@ var dy = -2;
 var ballRadius = 10;
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width.paddleWidth)/2;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
+document.addEventListener("keydown", keyDownHandler,false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+
+
+//공을 생성하는 함수
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x,y,ballRadius,0,Math.PI*2);
@@ -17,27 +43,42 @@ function drawBall(){
     ctx.closePath();
 }
 
-function draw(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+//공을 쳐주는 paddle 을 생성하는 함수
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//메인 함수
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
+    drawPaddle();
+
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
 
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
 }
 
-function drawPaddle (){
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth)
-}
 
-setInterval(draw,5);
+
+setInterval(draw,10); //setInterval 을 사용해 무한으로 draw 함수를 실행
 
 
 
